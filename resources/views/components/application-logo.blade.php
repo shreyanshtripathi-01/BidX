612406f30b49f2d7dca495d888f67e342c5eb8fc
Shreyansh Tripathi <Shreyanshcoder196@gmail.com>
2026-05-17 15:09:07 +0530

    style: implement centered typography branding for navbar

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,13 +1,6 @@
-<svg viewBox="0 0 120 40" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <!-- Icon part: Stylized luxury B & X fusion in champagne bronze -->
-    <g fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="text-amber-600 dark:text-[#C5A880]">
-        <!-- Luxury geometric emblem -->
-        <path d="M10 8h10c2.5 0 4.5 1.5 4.5 4s-2 4-4.5 4H10v-8z" />
-        <path d="M10 16h11c2.5 0 4.5 1.5 4.5 4s-2 4-4.5 4H10v-8z" />
-        <path d="M26 8l8 16" />
-        <path d="M34 8l-8 16" />
-    </g>
-    <!-- Text part: Figtree clean modern typography -->
-    <text x="48" y="25" font-family="'Figtree', sans-serif" font-weight="900" font-size="18" letter-spacing="0.5" class="fill-gray-900 dark:fill-zinc-100">Bid</text>
-    <text x="78" y="25" font-family="'Figtree', sans-serif" font-weight="900" font-size="18" letter-spacing="0.5" class="fill-amber-600 dark:fill-[#C5A880]">X</text>
+<svg viewBox="0 0 100 40" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <!-- Perfectly centered clean typographic logo with luxury style -->
+    <text x="50%" y="26" text-anchor="middle" font-family="'Figtree', sans-serif" font-weight="900" font-size="22" letter-spacing="0.5">
+        <tspan class="fill-gray-900 dark:fill-zinc-100">Bid</tspan><tspan class="fill-amber-600 dark:fill-[#C5A880]">X</tspan>
+    </text>
 </svg>
