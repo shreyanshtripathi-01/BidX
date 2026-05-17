c029e4e3008e45b559eb47ac1582cfd0ea7b9fa3
Shreyansh Tripathi <Shreyanshcoder196@gmail.com>
2026-05-17 15:22:41 +0530

    fix: clear build-time configuration cache programmatically on boot

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,10 @@
 <?php
 
+// Prevent Nixpacks build-time configuration cache poisoning by deleting the cache file programmatically on boot.
+if (file_exists(__DIR__.'/cache/config.php')) {
+    @unlink(__DIR__.'/cache/config.php');
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
